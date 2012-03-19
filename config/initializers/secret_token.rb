96718f2ace40afdb6e2085d98b378c33984bd592
Will McNeilly <wmcneilly@rumblelabs.com>
2012-03-19 20:47:49 +0000

    Initial commit, add devise model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatchTheWormRails::Application.config.secret_token = '98796c3904ae9150610695218fd83937a5372bd76bb9e1fce91fbd7fc650a01b8bb50c85ec7d370bc104cb1782398842d68d529efb9ad671a6ab9acc5e34bde1'
